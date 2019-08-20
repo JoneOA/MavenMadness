70e5e11347f514b14d2abf26d1d8f18c6dfe2f38
Owain <owainjones@acadamytrainee.com>
2019-08-20 13:57:28 +0100

    Added simple method and test for this method

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,5 +1,6 @@
 package com.qa;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,12 @@ public class AppTest
            app = new App();
            String[] input = {"a"};
            app.main(input);
+    }
+
+    @Test
+    public void simpleTest1() {
+       app = new App();
+       int total = app.simpleMethod(1,2);
+       assertEquals(total, 1);
     }
 }
